ae8316efd2d6c7af6839d7b35bf425f27a5c6cf4
atanaelgmc <atanael.costa@dayonesystem.com.br>
2024-06-06 15:53:24 -0300

    criando agenda

---
+++ resources/js/Pages/Agenda.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    agenda: {
+        type: String,
+        required: true,
+    }
+});
+
+const renderAgenda = () => {
+    return agenda.map((item)=>{
+        return (
+            <div key={{item.id}}>
+            </div>
+        );
+    });
+}
+</script>
+
+<template>
+    <Head title="Agenda" />
+
+        hello
+
+    <div>{{JSON.stringify(agenda)}}</div>
+</template>
